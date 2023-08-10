d14e0c69d581a5ffd262a83b3302231841b18676
Abdeljalil37 <widadpop@gmail.com>
2023-08-10 17:19:14 +0100

    add some libraries in header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+
+#define BUFSIZE 1024
+
+#endif
