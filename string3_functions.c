c9f0d782f94140d89e4c11ee6a32a9a97dcfc479
Abdeljalil37 <widadpop@gmail.com>
2023-08-21 23:22:03 +0100

    string functions 3

--- string3_functions.c
+++ string3_functions.c
@@ -1,68 +1,31 @@
+
 #include "main.h"
 
 /**
- *  _strcmp - compare to strings values
- * @str1: parameter of first string given
- * @str2: parameter of second string given
- *
- * Return: difference value ascii
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
  */
-int _strcmp(char *str1, char *str2)
+void rev_string(char *s)
 {
-	int len = 0;
+	int count = 0, i, j;
+	char *str, temp;
 
-	while (str1[len] != '\0' && str2[len] != '\0')
+	while (count >= 0)
 	{
-		if (str1[len] != str2[len])
-		{
-			return (str1[len] - str2[len]);
-		}
-		len += 1;
+		if (s[count] == '\0')
+			break;
+		count++;
 	}
-	return (0);
-}
+	str = s;
 
-/**
- * strtow - split string into array
- * @str: parameter of string to split
- * Return: return pointer to array
- */
-char **strtow(char *str)
-{
-	int i, row = 0, words;
-	char **ptr;
-
-	if (str == NULL)
-		return (NULL);
-	words = wdcount(str);
-	if (!words)
-		return (NULL);
-	ptr = malloc((words + 1) * sizeof(*ptr));
-	if (ptr == NULL)
-		return (NULL);
-	while (*str)
+	for (i = 0; i < (count - 1); i++)
 	{
-		if (*str == ' ')
+		for (j = i + 1; j > 0; j--)
 		{
-			str += 1;
-			continue;
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
 		}
-		for (i = 0; str[i] != ' ' && str[i] != '\0'; i++)
-			;
-		ptr[row] = malloc(i + 1);
-		if (ptr[row] == NULL)
-		{
-			for (i = 0; i < row; i++)
-				free(ptr[i]);
-			free(ptr);
-			return (NULL);
-		}
-		for (i = 0; str[i] != ' ' && str[i] != '\0'; i++)
-			ptr[row][i] = str[i];
-		ptr[row][i] = '\0';
-		row += 1;
-		str += i;
 	}
-	ptr[row] = NULL;
-	return (ptr);
 }
