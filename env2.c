bf14dfbde1c56fa9262701f30b5f4de1b7a822bb
shixyman <shixymanflowx@gmail.com>
2023-08-21 09:29:15 -0700

    add function compare env variable

--- env2.c
+++ env2.c
@@ -104,3 +104,23 @@ int _unsetenv(data_shell *datash)
 	datash->_environ = realloc_environ;
 	return (1);
 }
+/**
+ * _setenv - compares env variables names
+ * with the name passed.
+ * @datash: data relevant (env name and env value)
+ *
+ * Return: 1 on success.
+ */
+int _setenv(data_shell *datash)
+{
+
+	if (datash->args[1] == NULL || datash->args[2] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+
+	set_env(datash->args[1], datash->args[2], datash);
+
+	return (1);
+}
