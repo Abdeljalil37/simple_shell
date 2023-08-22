f4f5b2d481c0f1d3ef6362434441bb222d775385
Abdeljalil37 <widadpop@gmail.com>
2023-08-22 23:56:22 +0100

    finds index of first char

--- syntax_error_checker.c
+++ syntax_error_checker.c
@@ -67,3 +67,27 @@ int error_sep_op(char *input, int i, char last)
 
 	return (error_sep_op(input + 1, i + 1, *input));
 }
+
+/**
+ * first_char - finds index of the first char
+ *
+ * @input: input string
+ * @i: index
+ * Return: 1 if there is an error. 0 in other case.
+ */
+int first_char(char *input, int *i)
+{
+
+	for (*i = 0; input[*i]; *i += 1)
+	{
+		if (input[*i] == ' ' || input[*i] == '\t')
+			continue;
+
+		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
+			return (-1);
+
+		break;
+	}
+
+	return (0);
+}
